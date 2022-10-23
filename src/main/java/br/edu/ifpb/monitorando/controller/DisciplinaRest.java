7e7bbb0421e1cc450c156549105f2f75911d21d7
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-10-23 19:53:30 -0300

    adicionando endpoint de disciplinas e corrigindo os demais

--- src/main/java/br/edu/ifpb/monitorando/controller/DisciplinaRest.java
+++ src/main/java/br/edu/ifpb/monitorando/controller/DisciplinaRest.java
@@ -1,9 +1,29 @@
 package br.edu.ifpb.monitorando.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.edu.ifpb.monitorando.model.dto.DisciplinaDTO;
+import br.edu.ifpb.monitorando.model.service.DisciplinaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/disciplinas")
 public class DisciplinaRest {
+
+    @Autowired
+    private DisciplinaService disciplinaService;
+
+    @GetMapping(value = "/buscarDisciplinas/{nomeCurso}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<List<DisciplinaDTO>> getDisciplinasPorCurso(@PathVariable String nomeCurso){
+        List<DisciplinaDTO> disciplinas = disciplinaService.disciplinasPorCurso(nomeCurso);
+        if (disciplinas == null || disciplinas.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return  new ResponseEntity<>(disciplinas,HttpStatus.OK);
+    }
+
 }
