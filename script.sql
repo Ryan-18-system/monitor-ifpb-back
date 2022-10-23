19b3910e8c0bd920fefdf3971edbabe4fad652d0
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-10-23 19:54:26 -0300

    adicionando script de teste

---
+++ script.sql
@@ -0,0 +1,24 @@
+insert into tb_cursos values(default,'Thiago Moura', 'Sistemas Para Internet');
+insert into tb_cursos values(default,'Fernando Augusto Ferreira Lordao', 'Administração');
+insert into tb_cursos values(default,'Rafael Franklin Alves Silva', 'Automação Industrial');
+insert into tb_cursos values(default,'Walter Ladislau de Barros Ribeiro ', 'Construção de Edifícios');
+insert into tb_cursos values(default,'Marcio Emanuel Ugulino de Araujo Junior', 'Redes de Computadores');
+insert into tb_cursos values(default,'Robson Oliveira Lima ', 'Negócios Imobiliários');
+select * from tb_cursos;
+
+insert into tb_disciplinas values(default,'Fausto','POO',1);
+insert into tb_disciplinas values(default,'Gustavo','PWEB1',1);
+insert into tb_disciplinas values(default,'Fred','PWEB2',1);
+insert into tb_disciplinas values(default,'Luiz Carlos','Linguagem de Script',1);
+insert into tb_disciplinas values(default,'Alex','Estrutura de dados',1);
+
+select * from tb_disciplinas;
+
+insert into tb_alunos values(default,'joao@gmail.com','33439638014','João Doido',1);
+insert into tb_alunos values(default,'heitom80@eccotrans.com.br','21896991424','Heitor',2);
+insert into tb_alunos values(default,'abmoura83@infonet.com.br','06778734418','Fábio Benício Moura',1);
+insert into tb_alunos values(default,'giova@amordeconvite.com.br','61811160441','Giovana Analu Silva',1);
+
+select * from tb_alunos;
+
+
